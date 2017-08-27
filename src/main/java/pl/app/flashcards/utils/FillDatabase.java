98594c1e7396c1fb0fbb45de3d75a65c179b8a53
aniat61 <ania.trzeciak.3@wp.pl>
2017-08-27 16:08:10 +0200

    add new word

--- src/main/java/pl/app/flashcards/utils/FillDatabase.java
+++ src/main/java/pl/app/flashcards/utils/FillDatabase.java
@@ -9,12 +9,6 @@ import pl.app.flashcards.database.models.Category;
 import pl.app.flashcards.utils.exceptions.ApplicationException;
 
 
-/**
- * Created by ZacznijProgramowac.
- * https://www.youtube.com/zacznijprogramowac
- * http://zacznijprogramowac.net/
- * Służy jako wypełniacz bazy danych
- */
 public class FillDatabase {
     public static  void fillDatabase(){
         Category category1 = new Category();
@@ -41,13 +35,24 @@ public class FillDatabase {
         word5.setWord("fraction");
         word5.setDefinition("ułamek");
         word5.setCategoryID(category2);
+        Category category3= new Category();
+        category3.setName("Kolory");
+        Word word6 = new Word();
+        word6.setWord("green");
+        word6.setDefinition("zielony");
+        word6.setCategoryID(category3);
+        Word word7 = new Word();
+        word7.setWord("red");
+        word7.setDefinition("czerwony");
+        word7.setCategoryID(category3);
+
 
         CategoryDao categoryDao = new CategoryDao();
         try {
             categoryDao.creatOrUpdate(category2);
             DbManager.closeConnectionSource();
         } catch (ApplicationException e) {
-            e.printStackTrace();
+            DialogsUtil.errorDialog(e.getMessage());
         }
 
         WordDao wordDao = new WordDao();
@@ -57,8 +62,10 @@ public class FillDatabase {
             wordDao.creatOrUpdate(word3);
             wordDao.creatOrUpdate(word4);
             wordDao.creatOrUpdate(word5);
+            wordDao.creatOrUpdate(word6);
+            wordDao.creatOrUpdate(word7);
         } catch (ApplicationException e) {
-            e.printStackTrace();
+           DialogsUtil.errorDialog(e.getMessage());
         }
         DbManager.closeConnectionSource();
     }
